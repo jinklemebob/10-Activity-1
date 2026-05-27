d1501f3da7566f6feebf2839af9a35fc508948df
Pajamas <guantero.290593@balagtas.sti.edu.ph>
2026-05-27 18:35:24 +0800

    Add files via upload

---
+++ Admin-UserForm10 Activity 1/Administrator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity10
+{
+    internal class Administrator : User
+    {
+        private string admin_name;
+        public string Admin_name
+        {
+            get { return admin_name; }
+            set { admin_name = value; }
+        }
+        public Administrator(string name, string id, string pass) : base(id, pass)
+        {
+            this.admin_name = name;
+            this.user_id = id;
+            this.user_password = pass;
+
+        }
+        public override void updatepassword(string password)
+        {
+            this.user_password = password;
+        }
+        public string updateAdminName(string name)
+        {
+            return Admin_name = name;
+        }
+
+    }
+}
